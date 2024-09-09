7f509fa0de15a00e50ffd6b227997ba82fe6b049
mohamed elwardi <mohamedelwardi@yahoo.com>
2024-09-09 20:47:32 +0200

    solved the issue with the paths

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Blueprint
 
+template_dir = os.path.join(os.path.dirname(__file__), '../templates/auth')
 #create the blueprint for main
-auth_bp = Blueprint('auth', __name__,template_folder='templates/auth', url_prefix='/auth')
+auth_bp = Blueprint('auth', __name__,template_folder=template_dir, url_prefix='/auth')
 
 #Import routes so they are registred with the blueprints
 from app.auth import routes 
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Blueprint
 
+template_dir = os.path.join(os.path.dirname(__file__), '../templates/main')
 #create the blueprint for main
-main_bp = Blueprint('main', __name__,template_folder='templates/main', url_prefix='/')
+main_bp = Blueprint('main', __name__,template_folder=template_dir, url_prefix='/')
 
 #Import routes so they are registred with the blueprints
 from app.main import routes 
